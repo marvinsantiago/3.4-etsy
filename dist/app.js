ddce6cadcb86de52cadb688c8201b709c0d2409c
Marvin Santiago <marvin.santiago@ymail.com>
2015-07-20 15:04:22 -0500

    Build 0.0.7

--- dist/app.js
+++ dist/app.js
@@ -1,34 +1,31 @@
 var contentEl = document.querySelector('.listings');
 
-function registerByQuery (querySelector) {
-    var templateString = document.querySelector(querySelector).innerHTML;
-    templateString = templateString.replace('&gt;', '>');
+function registerByQuery(querySelector) {
+  var templateString = document.querySelector(querySelector).innerHTML;
+  templateString = templateString.replace('&gt;', '>');
 
-    return Handlebars.compile(templateString);
+  return Handlebars.compile(templateString);
 }
 
+function registerPartialByQuery(name) {
+  var templateString = document.querySelector('#' + name + '-partial').innerHTML;
+  templateString = templateString.replace('&gt;', '>');
 
-function registerPartialByQuery (name) {
-    var templateString = document.querySelector('#' + name + '-partial').innerHTML;
-    templateString = templateString.replace('&gt;', '>');
-
-    return Handlebars.registerPartial(name, templateString);
+  return Handlebars.registerPartial(name, templateString);
 }
 
 registerPartialByQuery('article');
 
-
-Handlebars.registerHelper('my-date', function (dateString) {
+Handlebars.registerHelper('my-date', function(dateString) {
 
 });
 
-
 var handlebarsTemplate = registerByQuery('#main-template');
 
-loadEtsy('whiskey', function (data) {
-    var allArticles = '';
+loadEtsy('whiskey', function(data) {
+  var allArticles = '';
 
-    allArticles += handlebarsTemplate(data);
+  allArticles += handlebarsTemplate(data);
 
-    contentEl.innerHTML = allArticles;
+  contentEl.innerHTML = allArticles;
 });
